6a3314e02e25372b5a9dc56c29a661e263ac3f6a
Minji Kim <minji9360@gmail.com>
2024-10-31 01:17:18 +0900

    잔여석이 없는 타임은 disabled 되도록 설정

--- js/register.js
+++ js/register.js
@@ -1,14 +1,34 @@
+window.seatInfo = window.seatInfo || loadSeatInfoFromCookie();
 window.selectedTimes = window.selectedTimes || {};
 
+function loadSeatInfoFromCookie() {
+    const cookies = document.cookie.split("; ");
+    const reservationDataCookie = cookies.find(cookie => cookie.startsWith("reservationData="));
+    if (!reservationDataCookie) {
+        console.warn("저장된 seatInfo 데이터가 없습니다.");
+        return {};
+    }
+
+    const reservationData = JSON.parse(decodeURIComponent(reservationDataCookie.split("=")[1]));
+    console.log("불러온 seatInfo 데이터:", reservationData.seatInfo);
+    return reservationData.seatInfo || {};
+}
+
 document.querySelectorAll("td[data-time]").forEach(cell => {
-    cell.addEventListener("click", () => {
-        const time = cell.getAttribute("data-time");
-        if (cell.classList.toggle("selected")) {
-            selectedTimes[time] = true;
-        } else {
-            delete selectedTimes[time];
-        }
-    });
+    const time = cell.getAttribute("data-time");
+
+    if (!seatInfo[time]) {
+        cell.classList.add("disabled");
+        cell.style.pointerEvents = "none";
+    } else {
+        cell.addEventListener("click", () => {
+            if (cell.classList.toggle("selected")) {
+                selectedTimes[time] = true;
+            } else {
+                delete selectedTimes[time];
+            }
+        });
+    }
 });
 
 function register() {
@@ -18,6 +38,7 @@ function register() {
         return;
     }
 
+    // 선택된 시간 목록을 알림으로 표시
     const selectedList = Object.keys(selectedTimes).join(", ");
     alert(`등록 완료: ${name}님, 선택 시간 - ${selectedList}`);
 }
