e3f440b532be0cccb0b933603f8cb84b598783aa
Minji Kim <minji9360@gmail.com>
2024-10-30 23:58:21 +0900

    최초 세팅 이후에 주소 접속 시 신청자 정보 탭이 최초 화면이 되도록 수정

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-const days = ["mon", "tue", "wed", "thu", "fri"];
-const times = [2, 2, 2, 2, 1];
-let settingData = {};
+function loadInitialPage() {
+    const cookies = document.cookie.split("; ");
+    const reservationDataCookie = cookies.find(cookie => cookie.startsWith("reservationData="));
+
+    if (reservationDataCookie)
+        loadPage('register.html', document.querySelector(".menu button:nth-child(2)"));
+    else
+        loadPage('setting.html', document.querySelector(".menu button:nth-child(1)"));
+}
 
 function loadPage(page, buttonElement) {
     fetch(page)
         .then(response => response.text())
         .then(data => {
             document.getElementById("content").innerHTML = data;
-            updateMenuSelection(buttonElement);
-
             if (page === 'setting.html') loadScript('js/setting.js');
+
+            updateMenuSelection(buttonElement);
         })
         .catch(error => {
             console.error("페이지 로드 중 오류 발생:", error);
         });
 }
 
-// 외부 스크립트를 동적으로 로드
 function loadScript(src) {
     const script = document.createElement("script");
     script.src = src;
@@ -29,7 +34,8 @@ function updateMenuSelection(selectedButton) {
         button.classList.remove("selected");
         button.classList.add("unselected");
     });
-
     selectedButton.classList.add("selected");
     selectedButton.classList.remove("unselected");
 }
+
+window.onload = loadInitialPage;
