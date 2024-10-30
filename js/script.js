6f2688f29f5980c337b21d1fdb9f8003c67dfbda
Minji Kim <minji9360@gmail.com>
2024-10-31 00:17:47 +0900

    쿠키 초기화 기능 추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const days = ["mon", "tue", "wed", "thu", "fri"];
+const times = [2, 2, 2, 2, 1];
+let settingData = {};
+
 function loadInitialPage() {
     const cookies = document.cookie.split("; ");
     const reservationDataCookie = cookies.find(cookie => cookie.startsWith("reservationData="));
@@ -37,5 +41,19 @@ function updateMenuSelection(selectedButton) {
     selectedButton.classList.add("selected");
     selectedButton.classList.remove("unselected");
 }
+
+function resetCookie() {
+    const confirmReset = confirm("모든 데이터가 리셋됩니다. 진행하시겠습니까?");
+
+    if (confirmReset) {
+        document.cookie = "reservationData=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+        alert("쿠키가 리셋되었습니다.");
+
+        loadInitialPage();
+    } else {
+        alert("리셋이 취소되었습니다.");
+    }
+}
+
 
 window.onload = loadInitialPage;
